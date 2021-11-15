395da9c021fc1c0f395a889ec791a39399ed22af
Jerome <ducampdorgasjerome@gmail.com>
2021-11-15 17:15:26 +0100

    const port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || "5000");
+const port = normalizePort(process.env.PORT || 5000);
 app.set("port", port);
 
 const errorHandler = (error) => {
